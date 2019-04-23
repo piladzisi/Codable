f1a82295ebb9c8e741159f84b4d0980d20ac0d95
Anna <piladzisi@gmail.com>
2019-04-23 12:55:30 +0300

    decoder.keyDecodingStrategy = .convertFromSnakeCase

--- Codable.playground/Pages/A Quick Peek.xcplaygroundpage/Contents.swift
+++ Codable.playground/Pages/A Quick Peek.xcplaygroundpage/Contents.swift
@@ -2,47 +2,52 @@ import Foundation
 
 let json = """
 {
-    "myname": "Anna",
+    "name": "Anna",
     "id": 1,
-    "role": "Developer"
+    "role": "Developer",
+    "start_date": "April 2012"
 }
 """.data(using: .utf8)!
 
-struct Employee: Decodable {
+struct Employee: Codable {
     let name: String
     let id: Int
     let role: String
+    let startDate: String
     
-    enum CodingKeys: String, CodingKey {
-        case name = "myname"
-        case id = "id"
-        case role = "role"
-   }
+//    enum CodingKeys: String, CodingKey {
+//        case name
+//        case id
+//        case role
+//        case startDate
+//   }
     
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.id = try container.decode(Int.self, forKey: .id)
-        self.role = try container.decode(String.self, forKey: .role)
-    }
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.name = try container.decode(String.self, forKey: .name)
+//        self.id = try container.decode(Int.self, forKey: .id)
+//        self.role = try container.decode(String.self, forKey: .role)
+//        self.startDate = try container.decode(String.self, forKey: .startDate)
+//    }
 }
 
 let decoder = JSONDecoder()
-
+decoder.keyDecodingStrategy = .convertFromSnakeCase
 let employee = try! decoder.decode(Employee.self, from: json)
 
 employee.name
 employee.id 
 
-extension Employee: Encodable {
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-        try container.encode(id, forKey: .id)
-        try container.encode(role, forKey: .role)
-    }
-}
+//extension Employee: Encodable {
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(name, forKey: .name)
+//        try container.encode(id, forKey: .id)
+//        try container.encode(role, forKey: .role)
+//    }
+//}
 let encoder = JSONEncoder()
+encoder.keyEncodingStrategy = .convertToSnakeCase
 let encodedEmployee = try! encoder.encode(employee)
 
 //print(encodedEmployee.stringDescription)
