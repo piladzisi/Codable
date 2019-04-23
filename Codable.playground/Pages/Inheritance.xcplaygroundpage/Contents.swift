ad594da92a07fa63afd732e6afb800a7f5ee70bd
Anna <piladzisi@gmail.com>
2019-04-23 18:29:32 +0300

    encoding/decoding with classes (inheritance)

--- Codable.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
+++ Codable.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
@@ -2,8 +2,11 @@ import Foundation
 
 let json = """
 {
-    "id": "ABCD",
-    "title": "Some Title"
+  "media" : {
+    "id" : "ABCD",
+    "title" : "Some Title"
+  },
+  "isbn" : "123-434-2323-2323"
 }
 """.data(using: .utf8)!
 
@@ -15,3 +18,32 @@ class Media: Codable {
 let decoder = JSONDecoder()
 let encoder = JSONEncoder()
 encoder.outputFormatting = .prettyPrinted
+
+let media = try! decoder.decode(Media.self, from: json)
+
+class Book: Media {
+   let isbn: String
+    enum BookCodingKeys: String, CodingKey {
+        case isbn
+        case media
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: BookCodingKeys.self)
+        self.isbn = try container.decode(String.self, forKey: .isbn)
+        try super.init(from: container.superDecoder(forKey: .media))
+    }
+    override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: BookCodingKeys.self)
+        try container.encode(isbn, forKey: .isbn)
+        try super.encode(to: container.superEncoder(forKey: .media))
+    }
+}
+
+let book = try! decoder.decode(Book.self, from: json)
+
+book.id
+book.title
+book.isbn
+
+print(try! encoder.encode(book).stringDescription)
